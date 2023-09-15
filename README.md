379a20e9182919a7607f600e637deee50bf79aa8
Toomas Vahter <toomas.vahter@gmail.com>
2023-09-15 11:17:06 +0300

    Add example project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SwiftUIKeyframeAnimationExample
+
+Read more: [Blog](https://augmentedcode.io/2023/09/18/animating-with-keyframe-animator-in-swiftui/)
---
+++ SwiftUIKeyframeAnimationExample/SwiftUIKeyframeAnimationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKeyframeAnimationExampleApp.swift
+//  SwiftUIKeyframeAnimationExample
+//
+//  Created by Toomas Vahter on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKeyframeAnimationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
